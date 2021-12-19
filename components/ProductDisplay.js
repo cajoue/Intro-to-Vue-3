ee8485a3ac1b2d40da0b4f2880dee264111ab2fb
cajoue <cajoue@users.noreply.github.com>
2021-12-19 17:41:17 +0100

    10 communicatinf events

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,14 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="deleteFromCart">
+          Delete item
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,8 +64,11 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
       },
+      deleteFromCart() {
+        this.$emit('delete-from-cart', this.variants[this.selectedVariant].id)
+    },
       updateVariant(index) {
           this.selectedVariant = index
       }
