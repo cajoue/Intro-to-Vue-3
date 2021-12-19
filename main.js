678edb56093cdc1fee55e085f402a36779dadfbb
cajoue <cajoue@users.noreply.github.com>
2021-12-19 10:35:26 +0100

    05 list rendering

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {'id': 3456, 'color': 'Blue'},
+                {'id': 3457, 'color': 'Green'},
+            ],
+            sizes: ['S', 'M', 'L'],
         }
     }
 })
